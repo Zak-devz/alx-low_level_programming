448d955e77517f5ceedc8fa0b96b0639485f6e81
root <zakariarassili@gmail.com>
2023-03-21 18:24:18 +0000

    last upd

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,11 @@ int print_last_digit(int num)
 {
 	int last;
 
-	if (num < 0)
+	last =  num % 10;
+	if (last < 0)
 	{
-		num = -num;
+		last = -last;
 	}
-	last = num % 10;
 	_putchar(last + '0');
 	return (last);
 }
