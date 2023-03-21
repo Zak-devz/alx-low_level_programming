b99d7266b880695ba50c2bc6a1599582a836b235
root <zakariarassili@gmail.com>
2023-03-21 17:03:41 +0000

    file update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,6 @@
  */
 int print_sign(int n)
 {
-	int j = -1;
 
 	if (n > 0)
 	{
@@ -20,7 +19,7 @@ int print_sign(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		return (j);
+		return (-1);
 	}
 	if (n == 0)
 	{
