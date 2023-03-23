50db8d10b4d752e2ee30ea95a0de7d820a46d13a
root <zakariarassili@gmail.com>
2023-03-23 18:00:17 +0000

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,6 +16,11 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+			i++;
+		}
 		if (i % 3 == 0)
 		{
 			printf("Fizz ");
@@ -24,11 +29,6 @@ int main(void)
 		if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			i++;
-		}
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
 			i++;
 		}
 		if (i > 100)
