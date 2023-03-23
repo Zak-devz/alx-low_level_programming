6a8e995e355f6a6709ba94fe8b87095570f301df
root <zakariarassili@gmail.com>
2023-03-23 15:12:20 +0000

    function that drag line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_line - Draws a straight line in the terminal.
+ *
+ * @n: The number of times the character _ should be printed.
+ *
+ * Return: void.
+ *
+ * Description: This function prints the character _ n times in the
+ *              terminal, followed by a newline character \n. It can
+ *              only use the _putchar() function to print. If n is 0
+ *              or less, the function only prints the newline character.
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		if (n >= 1)
+		{
+			_putchar('_');
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
