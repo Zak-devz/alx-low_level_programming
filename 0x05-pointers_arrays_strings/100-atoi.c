36ba25c91beca0c359f4a196333de18db0fc6e54
root <zakariarassili@gmail.com>
2023-03-27 20:05:13 +0000

    fuction that convert string to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,40 @@
 #include "main.h"
+
 /**
- * _atoi - converts string to integer
- * @s: the string
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- * Return: integer value of string
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	int i = 0, neg = 0, d;
-	int num = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (s[i])
+	while (s[c])
 	{
-		if (s[i] == '-')
+		if (s[c] == 45)
 		{
-			neg++;
+			min *= -1;
 		}
-		else if (_isdigit(s[i]))
+
+		while (s[c] >= 48 && s[c] <= 57)
 		{
-			while (_isdigit(s[i]))
-			{
-				d = (s[i] - '0');
-				d = neg % 2 ? -d : d;
-				num = num * 10 + d;
-				i++;
-			}
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
 			break;
 		}
-		i++;
+
+		c++;
 	}
-	return (num);
+
+	ni *= min;
+	return (ni);
 }
