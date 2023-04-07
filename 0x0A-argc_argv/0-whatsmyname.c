ddee1e92268e0507fd5c761294d33769d056afb7
root <zakariarassili@gmail.com>
2023-04-07 19:44:46 +0000

    function  that  prints prog name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,11 @@
  *
  * Return: 0
  */
-int main(int argc __attribute__((unused)), char* argv[])
+int main(int argc, char* argv[])
 {
+	if (argc > 0)
+	{
 		printf("%s\n", argv[0]);
+	}
 		return (0);
 }
