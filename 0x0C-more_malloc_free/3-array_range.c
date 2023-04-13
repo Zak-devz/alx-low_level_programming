440b946d1689e59c74574ab9c15410dfc4d80358
root <zakariarassili@gmail.com>
2023-04-13 16:18:15 +0000

    function that creates an array of integers

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if man > mix, returns NULL.
+ * if malloc fails, returns NULL.
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, size;
+
+	if (min > max)
+		return (NULL);
+	size = min + max + 1;
+	arr = malloc(sizeof(*arr) * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; min <= max; i++, min++)
+		arr[i] = min;
+	return (arr);
+}
+
